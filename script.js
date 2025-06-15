e2d5f0e2f3574e5f251d6c9fab665ff54c613657
Deanny-MrSuicide <yarynkagordynska2000@gmail.com>
2025-06-15 15:30:05 +0300

    sezr

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ function getWeather(city) {
         .then(d => {
             console.log(d)
             data = d
-            if(data.current.condition.code > 1000 && data.current.condition.code < 1050){
-                document.body.style.background = "url()"
+            if(data.current.condition.code > 1180 && data.current.condition.code < 1201){
+                document.body.style.backgroundImage = "url(https://static.wixstatic.com/media/5acbb4_68aecc720ed54b5a89edf655cb6f9f6e~mv2.gif)"
+            }
+            if(data.current.condition.code > 1210 && data.current.condition.code < 1225){
+                document.body.style.backgroundImage = "url(https://sanjuanheadwaters.org/wp-content/uploads/2023/02/snow-falling-gif.gif)"
+            }
+            if(data.current.condition.code = 1000){
+                document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/51/29/98/512998939eb4d7097d95feaf8d1cf5f4.gif)"
+            }
+            if(data.current.condition.code = 1003 && data.current.condition.code < 1030){
+                document.body.style.backgroundImage = "url(https://cdnimpuls.com/voxnews.al/media3/-640-0-giphy-1678084417.gif)"
             }
             document.querySelector(".city").innerHTML = data.location.name
             document.querySelector(".temp").innerHTML = "Температура: " + data.current.temp_c + "℃"
