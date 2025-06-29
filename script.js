705c11d9b0cbf4d0061eabaa96bfc11c6e63952e
Teremki16 <teremki16robocode@gmail.com>
2025-06-29 14:49:16 +0300

    sdfcghj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let data;
 
 function getWeather(city) {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=f030cb6a5047432399f111440250106&q=${city}&days=14&aqi=no&alerts=no`)
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=f030cb6a5047432399f111440250106&q=${city}&days=14&aqi=no&alerts=no`)
         .then(res => res.json())
         .then(d => {
             console.log(d)
@@ -29,7 +29,7 @@ function getWeather(city) {
             data.forecast.forecastday.forEach(el => {
                 document.querySelector(".days").innerHTML += `
                     <div class="day">
-                    <img src="${el.day.condition.icon}" alt="">
+                    <img src="https:/${el.day.condition.icon}" alt="">
                     <div class="maxt">Макс. ${el.day.maxtemp_c} ℃</div>
                     <div class="mint">Мін. ${el.day.mintemp_c} ℃</div>
                     <div class="uv">УФ - індекс ${el.day.uv}</div>
@@ -71,7 +71,7 @@ getWeather("Kyiv")
 
 document.querySelector(`#search`).addEventListener("input", (e) => {
     console.log(e.target.value)
-    fetch(`http://api.weatherapi.com/v1/search.json?key=f030cb6a5047432399f111440250106&q=${e.target.value}`).then(res => res.json()).then(data => {
+    fetch(`https://api.weatherapi.com/v1/search.json?key=f030cb6a5047432399f111440250106&q=${e.target.value}`).then(res => res.json()).then(data => {
         document.querySelector("#searching").innerHTML = data.map(c => `<option value="${c.name}">${c.name}</option>`).join("")
     })
 })
